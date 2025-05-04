b6381ffb33e9c3ce18fdff5b4cc70496e0e1e133
sky7997 <skyasarthetwothrones@gmail.com>
2025-05-04 18:43:33 +0530

    Update coffe_vend_mac(frnt,bacend).js

--- coffe_vend_mac(frnt,bacend).js
+++ coffe_vend_mac(frnt,bacend).js
@@ -1,184 +1,178 @@
 //App.js
 //App.js
-import React, {useState,useEffect} from "react"
+import React,{useState,useEffect} from "react"
+
 const App=()=>{
-  const [selectedCoffee, setSelectedCoffee] = useState("")
-  const [size,setSize]=useState("")
-  const [qnty,setQnty]=useState(1)
-  const [addOn,setAddOn]=useState([])
-  const [brew,setBrew]=useState(false)
-  const [ready,SetReady]=useState(false)
-  const [total,setTotal]=useState({subtotal:0, tax:0, total:0})
-const tax=0.10
-const [cofdat,setCofdat]=useState([])
-const [sizeDat,setSizedat]=useState([])
-const [addDat,setAddOndat]=useState([])
-
-
-  useEffect(()=>{
-    fetch("http://localhost:5000/data")
-    .then(res=>res.json())
-    .then(dat=>{
-      setCofdat(dat.coffeeOptions)
-      setSizedat(dat.sizes)
-      setAddOndat(dat.addOns)
- 
-    })
-  },[])
-
-const cofeChange=(e)=>{
-const cc=e.target.value
-setSelectedCoffee(cc)
-calculate(cc,size,qnty,addOn)
-}
-const sizechnge=(e)=>{
-  const sc=e.target.value
-  setSize(sc)
-  calculate(selectedCoffee,sc,qnty,addOn)
-}
-const onChnageqty=(e)=>{
-  const cq=parseInt(e.target.value)
-  setQnty(cq)
-  calculate(selectedCoffee,size,cq,addOn)
-}
-const addChange=(e)=>{
-  
-    const { value, checked } = e.target;
-    let newdat;
-    if (checked) {
-      newdat = [...addOn, value];
-      setAddOn(newdat); // Update the addOn state
-    } else {
-      newdat = addOn.filter(j => j !== value);
-      setAddOn(newdat); // Update the addOn state
-    }
-    calculate(selectedCoffee, size, qnty, newdat); // Use the updated newdat here
-  };
-  
-
-
-const brewCLick=()=>{
-if (!selectedCoffee || !size) {
-  alert("pls select")
-  return
-}
-setBrew(true)
-SetReady(false)
-setTimeout(()=>{
-  setBrew(false)
-  SetReady(true)
-},3000)
-}
-const clearCLick=()=>{
-  setSelectedCoffee("")
-  setSize("")
-  setQnty(1)
-  setAddOn([])
-  setBrew(false)
-  SetReady(false)
-  setTotal({subtotal:0, tax:0, total:0})
-}
-const calculate=(selectedCoffee,size,qnty,addOn)=>{
-  const s=cofdat.find(c=>c.name===selectedCoffee)
-  const sz=sizeDat.find(s=>s.name===size)
-  if (!s || !sz) return
-  const basePrice=s.basePrice*sz.multiplier
-  const addprice=addOn.reduce((sum,addOn)=>{
-    const f=addDat.find(j=>j.name===addOn)
-    return sum + (f? f.price:0)
-  },0)
-  const st=qnty*(basePrice+addprice)
-  const tx=st*tax
-  const totl=st+tx
-
-  setTotal({
-    subtotal:st.toFixed(2),
-    tax:tx.toFixed(2),
-    total:totl.toFixed(2)
+const [cofdat,setcofData]=useState([])
+const [sizedat,setSizedat]=useState([])
+const [addondat,setAddondat]=useState([])
+const [qnty,setQnty]=useState(1)
+const [cm,setC]=useState("")
+const [sm,setS]=useState("")
+const [am,setA]=useState([])
+const [brew,setBrew]=useState(false)
+const [ready,setReady]=useState(false)
+const [total,setTotal]=useState({subtotal:0,tax:0,total:0})
+const taxx=0.10
+useEffect(()=>{
+  fetch("http://localhost:5000/data")
+  .then(res=>res.json())
+  .then(dat=>{
+    setcofData(dat.cofeeOptions)
+    setSizedat(dat.sizes)
+    setAddondat(dat.addOns)
   })
+},[])
+const cc=(e)=>{
+  const aaa=e.target.value
+  setC(aaa)
+  calculation(aaa,sm,qnty,am)
+}
+const sc=(e)=>{
+const bbb=e.target.value
+setS(bbb)
+calculation(cm,bbb,qnty,am)
+}
+const qc=(e)=>{
+const qqq=parseInt(e.target.value)
+setQnty(qqq)
+calculation(cm, sm,qqq, am)
+}
+const ac=(e)=>{
+  const {value,checked}=e.target
+  let add;
+  if (checked) {
+    add=[...am,value]
+    setA(add)
+    calculation(add,cm,sm,qnty)
+  }
+  else {
+    add=am.filter(t=>t !== value)
+    setA(add)
+    calculation(cm,sm,qnty,add)
+  }
+ 
 }
 
-  return (
-    <div>
-      <div>
-        <h1>Coffee Vending MAchine</h1>
-        <label>selected coffee</label>
-        <select value={selectedCoffee} onChange={cofeChange}>
-        {cofdat.map((t,index)=>(
-          <option value={t.name} key={index}>{t.name} {t.basePrice}</option>
-        ))}
-        </select>
-        <label>Select Size</label>
-        {sizeDat.map((y,i)=>(
-          <div>
-          <label key={i}>
-          <input
-          type="radio"
-          value={y.name}
-          onChange={sizechnge}
-          checked={size===y.name}
-          />{y.name}</label>
-          </div>
-        ))}
-        <label>Select Quantity</label>
-        <input
-        type="number"
-        min="1"
-        value={qnty}
-        onChange={onChnageqty}
-        />
-        <label>Add ons</label>
-        { addDat.map((k,l)=>(
-          <label key={l}>
-            <input
-            type="checkbox"
-            value={k.name}
-            onChange={addChange}
-            checked={addOn.includes(k.name)}
-            />{k.name} (${k.price})
-          </label>
-        ))}
-        <h1>Order Summary</h1>
-        <p>subtotal : {total.subtotal}</p>
-        <p>tax : {total.tax}</p>
-        <p> total : {total.total}</p>
-        <div>
-          <button onClick={brewCLick}>Brew</button>
-          <button onClick={clearCLick}>Clear</button>
-          {brew && <p>Brewing your coffee please wait</p>}
-          {ready && <h1>your cofee is ready</h1>}
-        </div>
-      </div>
-    </div>
-  )
+const calculation=((cm,sm,qnty,am)=>{
+  const cof=cofdat.find(s=>s.name===cm)
+  const siz=sizedat.find(s=>s.name===sm)
+  if (!cof || !siz) return
+  const bp=cof.price * siz.multiplier
+  const adsum=am.reduce((sum,am)=>{
+    const f=addondat.find(t=>t.name===am)
+    return sum + (f? f.price :0)
+  },0)
+const st=qnty*(bp+adsum)
+const tx=st*taxx
+const tot=st+tx
+setTotal({
+  subtotal:st.toFixed(2),
+  tax:tx.toFixed(2),
+  total:tot.toFixed(2),
+})
+}
+)
+const bb=()=>{
+  if (!cm || !sm) {
+    alert("pls select")
+    return 
+  }
+  setBrew(true)
+  setReady(false)
+  setTimeout(()=>{
+    setBrew(false);
+    setReady(true)
+  },3000)}
+
+const cb=()=>{
+setBrew(false)
+setReady(false)
+  setC("")
+  setA([])
+  setS("")
+  setQnty(1)
+  setTotal({subtotal:0,tax:0,total:0})
+}
+return(
+  <div>
+    <h2>coffee vending machine</h2>
+    <p>select coffee</p>
+    <select onChange={cc} value={cm}>
+      <option>none</option>
+    {cofdat.map(t=>(
+      <option value={t.name} key={t.id}>{t.name} {t.price}</option>
+    ))}
+    </select>
+<div>
+  <p>sizes</p>
+{sizedat.map((t)=>(
+  <label key={t.id}>
+    <input
+    type="radio"
+    value={t.name}
+    checked={sm===t.name}
+  onChange={sc}
+    />
+    {t.name}
+  </label>
+))}
+</div>
+<p>Quantity</p>
+<input
+type="number"
+value={qnty}
+min="1"
+onChange={qc}
+
+/>
+<p>addons</p>
+{addondat.map((t)=>(
+  <label key={t.id}>
+    <input
+    type="checkbox"
+    value={t.name}
+    onChange={ac}
+    checked={am.includes(t.name)}
+    />
+    {t.name} {t.price}
+  </label>
+))}
+<p>total </p>
+<p>sub total {total.subtotal}</p>
+<p>tax{total.tax}</p>
+<p>total {total.total}</p>
+<button onClick={bb}>brew</button>
+<button onClick={cb}>clear</button>
+{brew && <p>brewing</p>}
+{ready && <p>take ur</p>}
+  </div>
+)
+
 }
 export default App
 
 
-
 //backend
 //Mockdata.js
-const coffeeOptions = [
-  { name: "Espresso", basePrice: 2 },
-  { name: "Cappuccino", basePrice: 3 },
-  { name: "Latte", basePrice: 3.5 },
-  { name: "Americano", basePrice: 2.5 },
+const cofeeOptions=[
+    {id:1, name:"tea", price:6},
+    {id:2, name:"blackTea", price:10},
+    {id:3, name:"coffee", price:8},
+    {id:4, name:"milk", price:5},
 ];
-
-const sizes = [
-  { name: "Small", multiplier: 1 },
-  { name: "Medium", multiplier: 1.5 },
-  { name: "Large", multiplier: 2 },
+const sizes=[
+    {id:1,name:"small",multiplier:1},
+    {id:2,name:"Medium",multiplier:2},
+    {id:3,name:"Large",multiplier:3},
 ];
-
-const addOns = [
-  { name: "Extra Shot", price: 0.7 },
-  { name: "Milk", price: 0.4 },
-  { name: "Sugar", price: 0.3 },
-  { name: "Whipped Cream", price: 0.6 },
+const addOns=[
+    {id:1,name:"withsugar",price:2},
+    {id:2,name:"withhoney",price:5},
+    {id:3,name:"withice",price:1},
+    {id:4,name:"withchocolate",price:8}
 ];
-
-module.exports={coffeeOptions,sizes,addOns};
+module.exports={cofeeOptions,sizes,addOns}
 
 //server.js
 const express=require("express")
@@ -187,13 +181,13 @@ const port=5000
 const app=express()
 app.use(cors())
 app.use(express.json())
-let {coffeeOptions,sizes,addOns}=require("./mockData")
+let {cofeeOptions,sizes,addOns}=require("./mockData")
 
 app.get("/", (req,res)=>{
   res.json("server running")
 })
 app.get("/data", (req,res)=>{
-  res.json({coffeeOptions,sizes,addOns})
+  res.json({cofeeOptions,sizes,addOns})
 })
 
 app.listen(port,()=>{
