80adfa67b4da2d0566e59c62e3a917b0abd6d0cd
sky7997 <skyasarthetwothrones@gmail.com>
2025-05-20 08:32:28 +0530

    Update fs node.js

--- fs nodejs.js
+++ fs nodejs.js
@@ -1,3 +1,5 @@
+// the difference between these two .txt and .json structures are we use stringify,parse in .json file type 
+//and in update operation in .json type we directly update remaining all same.
 //root map
 backend
   demo.txt
@@ -36,3 +38,41 @@ async function fileOperations() {
 }
 
 fileOperations();
+...............................................................................................................
+//root map
+backend
+  data.json //we use .json instead of .js because .js has code while .json has only data
+  server.js
+
+//server.js
+// fs with json file
+//we use .json instead of .js because .js has code while .json has only data
+const fs = require('fs').promises; 
+async function fileOperations() {
+  try {
+    // Read and parse JSON data
+    const fileContent = await fs.readFile('./data.json', 'utf-8');
+    //When you use 'utf-8' with fs.readFile, it tells Node.js to read the file as a text string, not as raw bytes (Buffer).
+    const jsonData = JSON.parse(fileContent); //converts json string into js object
+    console.log('Read JSON:', jsonData);
+
+    // Append new key-value to the object
+    jsonData.location = 'New York';
+
+    // Write updated JSON back to file
+    await fs.writeFile('./data.json', JSON.stringify(jsonData, null, 2)); // converts js object to json string
+    //null: the replacer (optional). null means "don't filter anything; include all keys".
+    //2: the space argument (also optional). It defines how many spaces to use for indentation in the output.
+    console.log('JSON File Updated Successfully');
+
+    // Delete the file
+    await fs.unlink('./data.json');
+    console.log('JSON File Deleted Successfully');
+
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+fileOperations();
+
