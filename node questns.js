3d2f9753453e1bd4429bb3d7aa064a26b124bc01
sky7997 <skyasarthetwothrones@gmail.com>
2025-05-20 12:14:52 +0530

    Update node questns.js

--- node questns.js
+++ node questns.js
@@ -20,4 +20,26 @@ app.post('/sum', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+-------------------------------------------------------------------------------------------------------------------------------------
+Q. Create a middleware in Express that checks if a request has an Authorization header. If not, return 401 Unauthorized.
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+const checkAuthHeader = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Unauthorized: Authorization header missing' });
+    }
+    next(); // Pass control to route
+  };
+  // Apply to specific route
+  app.get('/profile', checkAuthHeader, (req, res) => {
+    res.send('Welcome to your profile!');
+  });
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
 
